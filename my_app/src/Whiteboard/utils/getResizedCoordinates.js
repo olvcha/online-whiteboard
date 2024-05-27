10a12186469ddaf04ce25b8397b7b010786dcb3a
MiriAcer <miriam@wiber.pl>
2024-05-27 17:39:12 +0200

    fix move rectangle

--- my_app/src/Whiteboard/utils/getResizedCoordinates.js
+++ my_app/src/Whiteboard/utils/getResizedCoordinates.js
@@ -5,11 +5,13 @@ export const getResizedCoordinates = (x, y, position, coordinates) => {
     const { x1, y1, x2, y2 } = coordinates;
     switch (position) {
         case cursorPositions.TOP_LEFT:
-        case cursorPositions.BOTTOM_RIGHT:
             return { x1: x, y1: y, x2, y2 };
-        case cursorPositions.TOP_RIGHT:
-        case cursorPositions.BOTTOM_LEFT:
+        case cursorPositions.BOTTOM_RIGHT:
             return { x1, y1, x2: x, y2: y };
+        case cursorPositions.TOP_RIGHT:
+            return { x1 , y1: y, x2: x , y2 };
+        case cursorPositions.BOTTOM_LEFT:
+            return { x1: x, y1, x2, y2: y };
         default:
             return { x1, y1, x2, y2 };
     }
